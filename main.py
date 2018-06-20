cef751b6ffefdb3fffc990f2f636d058d8f922aa
samuel <chensrj2@gmail.com>
2018-06-20 16:39:48 -0500

    donoajdjfoad

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ class Queue(object):
 
     def put(self, x):
         self._q.append(x)
-        self._q.sort(key=lambda x: x[0])
-        self._q.reverse()
+        self._q.sort(key=lambda x: x[1])
 
     def get(self):
         x = self._q[0]
@@ -34,41 +33,36 @@ def createtree(frequencies):
     while p.qsize() > 1:        
         left, right = p.get(), p.get()
         node = HuffmanNode(left, right)
-        p.put((left[0]+right[0], node))      
+        holder = (node, int(left[1] + right[1]))
+        p.put(holder)      
     return p.get() 
 
 #get frequence
 freq = {}
-with open('C://Users//10111//documents//GitHub//HuffmanCoding//raw.txt','r') as raw:
-    for i in raw.read():
-        try:
-            freq[i]+=1
-        except:
-            freq[i] = 1
+filename = int(input('please type in the file name/path.'))
+try:
+    with open(filename,'rb') as raw:
+        for i in raw.read():
+            try:
+                freq[i]+=1
+            except:
+                freq[i] = 1
+except:print('unable to locate file')
 freq = sorted(freq.items(), key=operator.itemgetter(0))
 #create new tree
 
 node = createtree(freq)
 def walk_tree(node, prefix="", code={}):
     n = node
-    if isinstance(n, str):
-        code[n] = prefix
+    if isinstance(n[0], str):
+        code[n[0]] = prefix
     else:
-        try:
-            walk_tree(n.left, prefix + "0")
-            walk_tree(n.right, prefix + "1")
-        except:
-            code[n[0]] = prefix
+        walk_tree(n[0].left, prefix + "0")
+        walk_tree(n[0].right, prefix + "1")
     return code
 values = []
 keys = []
 code = walk_tree(node) 
-for key in code:
-    values += code[key]
-    keys += key
-values.reverse()
-for i in range(0,len(keys)):
-    code[i] = values[i]
 
 for i in sorted(freq, reverse=False):
     print(i[0], i[1], code[i[0]]) 
---
+++ text.py
@@ -0,0 +1,2 @@
+with open('C://Users//10111//documents//GitHub//HuffmanCoding//raw','rb') as raw:
+    print(raw.read())
